d07c02ef5665a94e18d4349c7270a548f8d31091
Fabricio <07fabricio07@gmail.com>
2025-10-09 23:36:47 -0500

    Descripción de los cambios realizados

--- src/Prestamos.js
+++ src/Prestamos.js
@@ -1,9 +1,3 @@
-import React from "react";
 export default function Prestamos() {
-  return (
-    <div>
-      <h2>Préstamos</h2>
-      {/* Aquí va la funcionalidad de préstamos */}
-    </div>
-  );
+  return null;
 }
